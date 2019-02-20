2b149e6a26765880b966fbaabcf7a5e8ee389da5
Yalin <yalin.gunayer@gmail.com>
2019-02-20 15:54:29 +0300

    Change Docker namespace

--- alpine-nodejs-nginx/build.sh
+++ alpine-nodejs-nginx/build.sh
@@ -5,4 +5,4 @@ set +a
 
 echo "Building the image version ${IMAGE_VERSION} with Node.js version ${NODEJS_VERSION}"
 
-docker build --build-arg IMAGE_VERSION="${IMAGE_VERSION}" --build-arg NODEJS_VERSION="${NODEJS_VERSION}" -t "unstatic.io/alpine-nodejs-nginx:${IMAGE_VERSION}" .
+docker build --build-arg IMAGE_VERSION="${IMAGE_VERSION}" --build-arg NODEJS_VERSION="${NODEJS_VERSION}" -t "unstaticio/alpine-nodejs-nginx:${IMAGE_VERSION}" .
